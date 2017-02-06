bd367674e1f2c953db6d4bd139a317ac38f6e042
NotMarko <Marko.Kasikovic@mail.mcgill.ca>
2017-02-06 16:52:16 -0500

    Updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,9 +23,8 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('LinkedIn', 'https://www.linkedin.com/in/marko-kasikovic-79761385'),
+         ('Github', 'https://github.com/Kasikov'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
